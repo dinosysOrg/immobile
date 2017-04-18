3a772f55f1eeb45be8af80e68471ca65b7a21005
Toan Tran <toantraz@mail.toantraz.com>
2017-04-18 11:20:30 +0700

    Created staging countries, cities, districts

--- install/sql/app/install/120_install_tables.sql
+++ install/sql/app/install/120_install_tables.sql
@@ -1,33 +1,53 @@
 \connect immobile
 
--- Table: measurement.source
+-- BEGIN STAGING.TABLES
 
--- DROP TABLE measurement.source;
+CREATE TABLE staging.countries
+(
+  identifier character varying(50) NOT NULL,
+  name character varying(255),
+  name_vi character varying(255),
+  lat double precision,
+  lon double precision,
+  CONSTRAINT countries_identifier_key UNIQUE (identifier)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE staging.countries
+  OWNER TO postgres;
 
--- CREATE TABLE settings.messages
--- (
---   id bigserial NOT NULL,
---   identifier character varying(50) NOT NULL,
---   name character varying(50) NOT NULL,
---   unit character varying(50),
---   category character varying(50),
---   value_type character varying(50),
---   format_string character varying(50),
---   is_calculated boolean DEFAULT false,
---   aggregation_func character varying(50),
---   cal_message_id1 integer,
---   cal_message_id2 integer,
---   cal_operator character varying(50),
---   default_value real,
---   module character varying(50),
---   CONSTRAINT pk_messages PRIMARY KEY (id),
---   CONSTRAINT messages_identifier_key UNIQUE (identifier)
--- )
--- WITH (
---   OIDS=FALSE
--- );
--- ALTER TABLE settings.messages
---   OWNER TO postgres;
+CREATE TABLE staging.cities
+(
+  country character varying(50) NOT NULL,
+  identifier character varying(255) NOT NULL,
+  name character varying(255),
+  name_vi character varying(255),
+  lat double precision,
+  lon double precision,
+  population integer,
+  CONSTRAINT staging_cities_key UNIQUE (identifier)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE staging.cities
+  OWNER TO postgres;
 
+CREATE TABLE staging.districts
+(
+  city character varying(255) NOT NULL,
+  identifier character varying(255) NOT NULL,
+  name character varying(255),
+  name_vi character varying(255),
+  lat double precision,
+  lon double precision,
+  CONSTRAINT staging_districts_key UNIQUE (identifier)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE staging.districts
+  OWNER TO postgres;
 
-
+-- END STAGING.TABLES
