73f4751a4742378a2488e530900d9af43e31405f
quytruong1991 <quytruong1991@gmail.com>
2017-04-21 15:25:29 +0700

    add qr code

--- webapp/app/controllers/home_controller.rb
+++ webapp/app/controllers/home_controller.rb
@@ -9,7 +9,13 @@ class HomeController < ApplicationController
   end
 
   def detail
-
+    if params[:id].to_i == 1
+      @coverUrl = "/assets/khanhcasa.jpg"
+      @matterUrl = "https://my.matterport.com/show/?m=f8Er8NwRrDs"
+    else
+      @coverUrl = "/assets/canho1.jpg"
+      @matterUrl = "https://my.matterport.com/show/?m=b7TQHKcT9oY"
+    end
   end
 
   def contact
--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'search', to: 'home#search'
-  get 'detail', to: 'home#detail'
+  get 'detail/:id', to: 'home#detail'
   get 'contact', to: 'home#contact'
 
 end
