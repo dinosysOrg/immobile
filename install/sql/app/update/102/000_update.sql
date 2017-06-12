120f89506631f28239b7f675a867d2619a840caf
Toan Tran <toantraz@mail.toantraz.com>
2017-06-12 15:08:30 +0700

    Added update to create spatial_areas tables

---
+++ install/sql/app/update/102/000_update.sql
@@ -0,0 +1,3 @@
+\echo 'Added spatial_areas tables in staging and settings...'
+\i update/101/100_create_spatial_areas_tables.sql
+
---
+++ install/sql/app/update/102/100_create_spatial_areas_tables.sql
@@ -0,0 +1,25 @@
+\connect immobile
+
+CREATE TABLE IF NOT EXISTS staging.spatial_areas (
+  identifier varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  area text NOT NULL,
+  CONSTRAINT pk_staging_spatial_areas PRIMARY KEY (identifier)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE staging.spatial_areas
+  OWNER TO postgres;
+  
+CREATE TABLE IF NOT EXISTS settings.spatial_areas (
+  id bigserial NOT NULL,
+  identifier varchar(255) NOT NULL UNIQUE,
+  name varchar(255) NOT NULL,
+  area GEOGRAPHY NOT NULL,
+  CONSTRAINT pk_settings_spatial_areas PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
