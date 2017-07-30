725e671ebcd0051c0ca436646dbe0bf54f0d400e
T.Tin Nguyen <tinnguyen911@gmail.com>
2017-07-31 01:56:34 +0700

    simplify routes

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,35 +1,43 @@
 Rails.application.routes.draw do
-  # Profile
-  get 'profile/post/new', to: 'profile#new_house'
-  get 'profile/post/:link', to: 'profile#edit_house'
-  get 'profile/post/:link/photo', to: 'profile#edit_photo'
-  post 'profile/post/new', to: 'profile#post_house'
-  post 'profile/post/:id', to: 'profile#put_house'
-  post 'profile/post/:id/photo', to: 'profile#put_photo'
-  delete 'profile/post/:id', to: 'profile#delete_house'
-  get 'profile/posts', to: 'profile#list_house'
-  get 'profile/edit', to: 'profile#edit_profile'
-  post 'profile/edit', to: 'profile#put_profile'
-  get 'profile/budget', to: 'profile#show_budget'
+
+  resources 'profile', except: [:show] do
+    collection do
+      get 'post/new', to: 'profile#new_house'
+      get 'post/:link', to: 'profile#edit_house'
+      get 'post/:link/photo', to: 'profile#edit_photo'
+      post 'post/new', to: 'profile#post_house'
+      post 'post/:id', to: 'profile#put_house'
+      post 'post/:id/photo', to: 'profile#put_photo'
+      delete 'post/:id', to: 'profile#delete_house'
+      get 'posts', to: 'profile#list_house'
+      get 'edit', to: 'profile#edit_profile'
+      post 'edit', to: 'profile#put_profile'
+      get 'budget', to: 'profile#show_budget'
+    end
+  end
 
   # Admin
-  get 'profile/users', to: 'admin#list_user'
-  post 'profile/user/:id/role', to: 'admin#edit_role'
-  get 'profile/user/:id', to: 'admin#edit_user'
-  post 'profile/user/:id/edit', to: 'admin#put_user'
-  post 'profile/user/:id/is_home', to: 'admin#is_home_user'
-  post 'profile/post/:id/status', to: 'admin#status_post'
-  post 'profile/post/:id/is_home', to: 'admin#is_home_post'
-  get 'profile/projects', to: 'admin#list_project'
-  get 'profile/project/new', to: 'admin#new_project'
-  get 'profile/project/:id', to: 'admin#edit_project'
-  get 'profile/project/:id/photo', to: 'admin#edit_project_photo'
-  post 'profile/project/:id/edit', to: 'admin#put_project'
-  post 'profile/project/:id/photo', to: 'admin#put_project_photo'
-  delete 'profile/project/:id', to: 'admin#delete_project'
-  post 'profile/project', to: 'admin#post_project'
-  post 'profile/project/:id/is_home', to: 'admin#is_home_project'
-  post 'profile/budget/callback', to: 'admin#callback_budget'
+  resources 'profile', except: [:show] do
+    collection do
+      get 'users', to: 'admin#list_user'
+      post 'user/:id/role', to: 'admin#edit_role'
+      get 'user/:id', to: 'admin#edit_user'
+      post 'user/:id/edit', to: 'admin#put_user'
+      post 'user/:id/is_home', to: 'admin#is_home_user'
+      post 'post/:id/status', to: 'admin#status_post'
+      post 'post/:id/is_home', to: 'admin#is_home_post'
+      get 'projects', to: 'admin#list_project'
+      get 'project/new', to: 'admin#new_project'
+      get 'project/:id', to: 'admin#edit_project'
+      get 'project/:id/photo', to: 'admin#edit_project_photo'
+      post 'project/:id/edit', to: 'admin#put_project'
+      post 'project/:id/photo', to: 'admin#put_project_photo'
+      delete 'project/:id', to: 'admin#delete_project'
+      post 'project', to: 'admin#post_project'
+      post 'project/:id/is_home', to: 'admin#is_home_project'
+      post 'budget/callback', to: 'admin#callback_budget'
+    end
+  end
 
   # Photo
   get 'photo/:user_id/:name', to: 'photo#load_avatar'
